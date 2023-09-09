24c09aadc739a9b085b511b628da951cea2a6d7b
amarragu <106455239+amarragu@users.noreply.github.com>
2023-09-09 09:38:26 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,116 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Jodi Nilave Song",
+    artist: "Thangamagan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Kaattumalli Song",
+    artist: "Viduthalai",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Enadhuyire Song",
+    artist: "Bheema",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Oru Murai Piranthen Song",
+    artist: "Nenjirikkum varai",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Endha Pakkam Song",
+    artist: "Dharmadurai",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Sollitaley Ava Kaadhala Song",
+    artist: "Kumki",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Oru Naalaikkul Song",
+    artist: "Yaaradi Nee Mohini",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Vaa Vaathi Song",
+    artist: "Vaathi",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Naan Pogiren",
+    artist: "Naanayam",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Usure Pogudhey Song",
+    artist: "Raavanan",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Idhuthaanaa Song",
+    artist: "Saamy",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Oliyile Therivadhu Devadhaya Song",
+    artist: "Azhagi",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Arabu Naade Song",
+    artist: "Thottal Poo Malarum",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Suthuthey Suthuthey Song",
+    artist: "Paiya",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Aagaya Suriyanai Song",
+    artist: "Samurai",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Eraivanai Thandha Song",
+    artist: "Velai Illa Pattadhaari 2",
+    img: "music-16",
+    src: "music-16"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
